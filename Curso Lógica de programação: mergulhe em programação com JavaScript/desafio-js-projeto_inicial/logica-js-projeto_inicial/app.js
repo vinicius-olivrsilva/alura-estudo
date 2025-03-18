d04e854143de4551069d256acbcce0c85b6a5d24
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-03-18 13:13:34 -0300

    Concluido primeiro capitulo curso de logica

---
+++ Curso Lógica de programação: mergulhe em programação com JavaScript/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,13 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = prompt("Qual o nome do usuário?");
+let idade = prompt("Qual a sua idade?");
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+    let mensagemDeErro = "Erro! Preencha todos os campos";
+    alert (mensagemDeErro);
+
+if (idade >= 18){
+    alert(nome + " " + idade + " anos " + "pode tirar a habilitação!")
+}
---
+++ Curso Lógica de programação: mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert("Jogo do numero secreto");
+let numeroSecreto = 7;
+let chute = prompt ("Escolha um número entre 1 e 10");
+
+
+if(numeroSecreto == chute) {
+    console.log("Acertou!");
+}
