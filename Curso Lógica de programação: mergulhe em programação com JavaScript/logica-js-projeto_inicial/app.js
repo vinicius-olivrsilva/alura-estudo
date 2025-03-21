45aa60c175909947a3a52ba55d6a5fd888a9cd39
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-03-21 12:34:52 -0300

    Conclusão do capitulo 4

--- Curso Lógica de programação: mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
+++ Curso Lógica de programação: mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
@@ -1,21 +1,34 @@
 alert("Jogo do numero secreto");
-let numeroSecreto = 4;
+let numeroSecreto = parseInt(Math.random()*100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 
 while(chute != numeroSecreto){
-    chute = prompt ("Escolha um número entre 1 e 10")
+    chute = prompt ("Escolha um número entre 1 e 100")
 
     if(numeroSecreto == chute) {
-        alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
-        if (chute > numeroSecreto){
-        alert(`O número secreto é menor que ${chute}`);
-        } else {
-        alert(`O número secreto é maior que ${chute}`);
-        }
+        let maiorMenor = chute > numeroSecreto ? "menor" : "maior";
+        alert(`O número secreto é ${maiorMenor} que ${chute}`);
+
+        // if (chute > numeroSecreto){
+        // alert(`O número secreto é menor que ${chute}`);
+        // } else {
+        // alert(`O número secreto é maior que ${chute}`);
+        // }
+        
         tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1) {
+//     alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+// } else {
+//     alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+// }
