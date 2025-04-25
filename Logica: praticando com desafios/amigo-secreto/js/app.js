de161a569b132fc8118811ba5b15992c7b4815b7
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-04-25 20:40:42 -0300

    Conclusão do Curso de Lógica com desafios

--- Logica: praticando com desafios/amigo-secreto/js/app.js
+++ Logica: praticando com desafios/amigo-secreto/js/app.js
@@ -1,18 +1,32 @@
 let listaAmigos = [];
 
 function adicionar(){
-    const elementoNome = document.getElementById('nome-amigo');
-    const nome = elementoNome.value;
-    let amigos = document.getElementById('lista-amigos');
+    const elementoNome = document.getElementById('nome-amigo'); // Coleta o nome digitado no input
+    const nome = elementoNome.value;                            // Adicina o nome em uma variável
+    let amigos = document.getElementById('lista-amigos'); 
+    
+    if (nome == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+    
+    if (listaAmigos.includes(nome)) {
+        alert('Nome já adicionado!');
+        return;
+    }
 
-    listaAmigos.push(nome);
-    amigos.textContent = listaAmigos;
+    listaAmigos.push(nome);                     // Adiciona o nome na lista de amigos
+    amigos.textContent = listaAmigos;           // ao mesmo tempo adicionando o nome no label da tela
 
-    elementoNome.value = '';
+    elementoNome.value = '';                    // limpa o input para recever outro nome
 }
 
 function sortear(){
-    embaralha(listaAmigos);
+    if (listaAmigos.length < 4) {
+        alert("Adicione pelo menos 4 amigos!")
+        return;
+    }
+    embaralha(listaAmigos);                     // função que embaralha os nomes dentro da lista (embalhara a ordem de indices do array)
     let sorteio = document.getElementById('lista-sorteio');
     for(let i = 0;  i < listaAmigos.length; i++) {
         if (i == listaAmigos.length - 1) {
