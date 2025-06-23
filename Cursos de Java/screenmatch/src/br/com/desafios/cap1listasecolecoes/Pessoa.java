74599d1ccfd764998eb085dfad1639856fa3fd0a
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-06-23 05:17:55 -0300

    Curso Java trabalhando com listas e coleções de dados - Capitilo 1

---
+++ Cursos de Java/screenmatch/src/br/com/desafios/cap1listasecolecoes/Pessoa.java
@@ -0,0 +1,19 @@
+package br.com.desafios.cap1listasecolecoes;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
--- Cursos de Java/screenmatch/src/br/com/screenmatch/Main.java
+++ Cursos de Java/screenmatch/src/br/com/screenmatch/Main.java
@@ -6,23 +6,27 @@ import br.com.screenmatch.modelos.Episodio;
 import br.com.screenmatch.modelos.Filme;
 import br.com.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("Matrix");
-        meuFilme.setAnoDeLancamento(1970);
-        meuFilme.setDuracaoEmMinutos(180);
-        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+        Filme matrix = new Filme();
+        matrix.setNome("Matrix");
+        matrix.setAnoDeLancamento(1970);
+        matrix.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + matrix.getDuracaoEmMinutos());
+        System.out.println("------------------------------------");
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
-        //meuFilme.somaDasAvaliacoes = 10;
-        //meuFilme.totalDeAvaliacoes = 1;
-        //System.out.println(meuFilme.pegaMedia());
+        matrix.exibeFichaTecnica();
+        matrix.avalia(8);
+        matrix.avalia(5);
+        matrix.avalia(10);
+        System.out.println("Total de avaliações: " + matrix.getTotalDeAvaliacoes());
+        System.out.println(matrix.pegaMedia());
+        //matrix.somaDasAvaliacoes = 10;
+        //matrix.totalDeAvaliacoes = 1;
+        //System.out.println(matrix.pegaMedia());
+        System.out.println("------------------------------------");
 
         Serie witcher = new Serie();
         witcher.setNome("The Witcher");
@@ -31,7 +35,8 @@ public class Main {
         witcher.setTemporadas(10);
         witcher.setEpisodiosPorTemporada(10);
         witcher.setMinutosPorEpisodio(50);
-        System.out.println("Duração para maratonar The Witcher: " + witcher.getDuracaoEmMinutos());
+        System.out.println("Duração para maratonar " + witcher.getNome() + ": " + witcher.getDuracaoEmMinutos());
+        System.out.println("------------------------------------");
 
         Filme duna = new Filme();
         duna.setNome("Duna");
@@ -39,14 +44,16 @@ public class Main {
         duna.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
-        calculadora.inclui(meuFilme);
+        calculadora.inclui(matrix);
         calculadora.inclui(duna);
         calculadora.inclui(witcher);
         System.out.println(calculadora.getTempoTotal());
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
-        filtro.filtra(meuFilme);
+        filtro.filtra(matrix);
         filtro.filtra(duna);
+
+        System.out.println("------------------------------------");
 
         Episodio episodio = new Episodio();
         episodio.setNome("Prólogo");
@@ -54,5 +61,26 @@ public class Main {
         episodio.setSerie(witcher);
         episodio.setTotalVisualizacoes(325);
         filtro.filtra(episodio);
+
+        System.out.println("------------------------------------");
+
+        var senhorDosAneis = new Filme();
+        senhorDosAneis.setNome("O Senhor dos Anéis: As Duas Torres");
+        senhorDosAneis.setDuracaoEmMinutos(223);
+        senhorDosAneis.setDiretor("Peter Jackson");
+        senhorDosAneis.setIncluidoNoPlano(true);
+        senhorDosAneis.setAnoDeLancamento(2002);
+        senhorDosAneis.avalia(8.7);
+        senhorDosAneis.exibeFichaTecnica();
+        System.out.println("------------------------------------");
+
+        // Curso Alura - Java: Trabalhando com listas e coleções de dados
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(senhorDosAneis);
+        listaDeFilmes.add(duna);
+        listaDeFilmes.add(matrix);
+        System.out.println("Tamanho da Lista de filmes: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println("Lista:\n" + listaDeFilmes);
     }
 }
