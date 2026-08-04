041054e0f10c1275d76a183c6a7fc4f5f1d5c642
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2026-08-04 19:17:53 -0300

    Lista Top 5 Episódios de uma série usando JPQL

--- cursos-java/spring-data-jpa/screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ cursos-java/spring-data-jpa/screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -16,6 +16,7 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
     private List<DadosSerie> dadosSerie = new ArrayList<>();
+    private Optional<Serie> serieEncontrada;
 
     private SerieRepository repositorio;
 
@@ -39,6 +40,7 @@ public class Principal {
                     7 - Buscar séries por categoria
                     8 - Filtrar séries
                     9 - Buscar episódio por trecho
+                    10- Listar Top 5 Episódios de uma série
                     
                     0 - Sair
                     """;
@@ -74,6 +76,9 @@ public class Principal {
                     break;
                 case 9:
                     buscarEpisodiosPorTrecho();
+                    break;
+                case 10:
+                    topEpisodiosPorSerie();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -162,7 +167,7 @@ public class Principal {
         System.out.println("Qual série deseja buscar? ");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serieEncontrada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+        serieEncontrada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
         if (serieEncontrada.isPresent()) {
             System.out.println("Dados da Série: " + serieEncontrada.get());
@@ -232,5 +237,17 @@ public class Principal {
         episodiosEncontrados.forEach(e ->
             System.out.printf("Série: %s - S%sE%s - %s\n",
                     e.getSerie().getTitulo(), e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()));
+    }
+
+    private void topEpisodiosPorSerie() {
+        buscarSeriePeloTitulo();
+        if (serieEncontrada.isPresent()) {
+            Serie serie = serieEncontrada.get();
+            List<Episodio> topEpisodios = repositorio.topEpisodiosPorSerie(serie);
+            topEpisodios.forEach(e ->
+                    System.out.printf("Série: %s - S%sE%s - %s - imdb: %.1f\n",
+                        e.getSerie().getTitulo(), e.getTemporada(),
+                            e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        } // não precisa fazer o else, pois o buscarSeriePeloTitulo(); já faz esse precesso caso não seja encontrada
     }
 }
